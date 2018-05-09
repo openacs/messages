196476aa4e9e99f9378a4fdcb1dedc561069d117
Hector Romojaro <hector.romojaro@gmail.com>
2018-05-09 15:33:32 +0000

    Revert massive replacement of empty list creation sentences. The use of '[list]' instead of '{}' adds semantics that could be used for performance improvements in the future, such as using a different internal representation. There is already work in this direction, avoiding the generation of the string representation during comparison of empty strings (huge thanks to Stefan Sobernig for the pointer: https://core.tcl.tk/tcl/info/44527c632ed609c2).

--- www/update-numbers.tcl
+++ www/update-numbers.tcl
@@ -14,7 +14,7 @@ append not_folder_ids [messages::get_folder_id -folder_name "sent"] "," \
 set user_id [ad_conn user_id]
 set community_id [dotlrn_community::get_community_id]
 set folders_info [db_list_of_lists count_new_messages {}]
-set numbers {}
+set numbers [list]
 foreach folder $folders_info {
     lappend numbers [lindex $folder 0]
     lappend numbers [lindex $folder 1]
